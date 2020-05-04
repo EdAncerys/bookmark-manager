2b71b1f46b202d2de2ad5d61914cef743d4050f8
mbrad26 <mbrad26@gmail.com>
2020-05-04 16:42:27 +0100

    interacting with PostgresQL

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -8,8 +8,8 @@ feature 'Viewing bookmarks' do
   scenario 'a user can see bookmarks' do
     visit('/bookmarks')
 
-    expect(page).to have_content 'http://www.makersacademy.com'
-    expect(page).to have_content 'http://www.makersacademy.com'
+    expect(page).to have_content 'http://www.twitter.com'
     expect(page).to have_content 'http://www.google.com'
+    expect(page).to have_content 'http://www.destroyallsoftware.com'
   end
 end
