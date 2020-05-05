3cb1003879fcd017bb208cf895734d5356d232ee
Ed <lookatemail@gmail.com>
2020-05-05 15:27:18 +0100

    Creating bookmarks

--- app.rb
+++ app.rb
@@ -8,9 +8,18 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks' do
     p ENV
-    
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
+  end
+
+  get '/bookmarks/new' do
+    erb :'bookmarks/new'
+  end
+
+  post '/bookmarks' do
+    p params 
+    Bookmark.create(url: params['url'])
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
