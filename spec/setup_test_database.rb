d6706c305e3999d567157aa4e22c675d6126dfec
Ed <lookatemail@gmail.com>
2020-05-07 11:45:20 +0100

    REST and Resources

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -5,6 +5,6 @@ p "Setting up test database..."
 def setup_test_database 
   connection = PG.connect(dbname: 'bookmark_manager_test')
   # Clear the bookmarks table
-  connection.exec("TRUNCATE bookmarks;")
+  connection.exec("TRUNCATE bookmarks, comments;")
 end
 
